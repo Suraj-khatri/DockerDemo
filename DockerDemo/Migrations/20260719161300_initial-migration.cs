ea42c9193c225eae0c1de9fccd6072a267f7d29a
suraj khatri <surajpoudelkhatri@gmail.com>
2026-07-19 22:02:33 +0545

    added docker-compose

--- DockerDemo/Migrations/20260719161300_initial-migration.cs
+++ DockerDemo/Migrations/20260719161300_initial-migration.cs
@@ -5,7 +5,7 @@
 namespace DockerDemo.Migrations
 {
     /// <inheritdoc />
-    public partial class InitialCreate : Migration
+    public partial class initialmigration : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
